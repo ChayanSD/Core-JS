d041b2ab479b05af3320e4b9b933ce2de80d2915
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-08-13 16:05:12 +0600

    conversion

---
+++ conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+console.log(typeof(score))
+
+let valueInNum = Number(score) // convert the string to num;
+
+console.log(score) //Ok  ! covert string to int But if the string is 33ab 
+//In that case , the output comes NaN means -> not a number . So be careFull.
+
+/*
+"44" -> 44
+"44abc" -> NaN
+1 -> true 0 ->false
+"" -> false
+"chayan" -> true 
+null -> 0
+*/
+
+// ************ Operations ***********************
+
+console.log(1 + 1 );
+//All this types of things.
