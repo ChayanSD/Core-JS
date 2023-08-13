74ed1f77ba3e2e567a46becbdfaba45c0d818c55
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-08-13 14:40:43 +0600

    dataTypes

---
+++ Day-1/data_types.js
@@ -0,0 +1,15 @@
+"use strict"  // treat all JS code as newer version
+
+//  alert( 3 + 3) ;   // It's now work in code editor , cause we are using node js , not browser.
+
+let name = "chayan"
+let age = 20
+let isLogIn = false
+
+//number , bigint , string , boolean 
+// null => standalone value(empty) and this an object.
+//  undefined =>  Any variable that has not been assigned a value has the value undefined.
+// symbol  => unique
+// object => 
+
+// console.log(typeof name)   we can see our variable type using typeof.
